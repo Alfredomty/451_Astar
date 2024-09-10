fc4f649368a20e77b2cc8346829599a5775d5bc8
Alfredomty <angelgranocruz@gmail.com>
2024-09-09 22:19:48 -0700

    ported over equation for the haversine distance

--- city_graph.py
+++ city_graph.py
@@ -27,3 +27,27 @@ class CityGraph:
                 starting_city = line_parts[0] #['SanJose']
                 neighbors = line_parts[1].split(',') #The rest
 
+                graph[starting_city] = {} # Populates the main graph
+                for neighbor in neighbors:
+                    dest, dist = neighbor.split('(')
+                    dist = float(dist.strip(')'))
+                    graph[starting_city][dest] = dist
+
+        return graph
+    
+    def haversine_distance(self, city1, city2):
+        lat1, lon1 = self.coords[city1]
+        lat2, lon2 = self.coords[city2]
+
+        radius = 3958.8
+
+        lat1, lon1, lat2, lon2 =  map(np.radians, [lat1, lon1, lat2, lon2]) # Switching them to radians
+
+        distance_between_lat = lat2 - lat1
+        distance_between_lon = lon2 - lon1
+
+        A = np.sin(distance_between_lat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(distance_between_lon / 2) ** 2
+        d = radius * 2 * np.arcsin(np.sqrt(A))
+
+        return d
+
