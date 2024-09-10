96ff9869517f6fdef31cfb4da58e3c3cbcc42ed7
Alfredomty <angelgranocruz@gmail.com>
2024-09-09 22:26:10 -0700

    added an algo class

---
+++ astar_algo.py
@@ -0,0 +1,8 @@
+
+class AStarAlgo:
+    # Will take the city graph and do a search on it
+    def __init__(self, city_graph):
+        self.city_graph = city_graph
+
+    def search():
+        pass
