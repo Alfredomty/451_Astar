43d796b6ba8f8a79eb2a64f7e2d67c63184b53ad
Alfredomty <angelgranocruz@gmail.com>
2024-09-10 20:10:42 -0700

    finished algo. Added arg passing to main. Debugging

--- a_star.py
+++ a_star.py
@@ -1,7 +1,30 @@
+import sys
 from a_star_algo import AStarAlgo
 
 # I know that I want to pass the args to main
 def main(start_city, goal_city):
-    pass
+    astar = AStarAlgo("coordinates.txt", "map.txt")
+
+    path, distance = astar.search(start_city, goal_city)
+
+    # Print statements
+    print(f"From city: {start_city}\nTo city: {goal_city}")
+
+    if path:
+        print(f"Best Route: {start_city} to {goal_city}: {' -> '.join(path)}")
+        print(f"Total distance: {distance:.2f} miles")
+    else:
+        print(f"No path found from {start_city} to {goal_city}")
+
 if __name__ == "__main__":
-    main()
+
+    # Error check for args
+    if len(sys.argv) != 3:
+        print("Not enough arguments (expected 3)")
+        print("Usage: python a_star.py <start_city> <goal_city>")
+        sys.exit(1)
+    
+    start_city = sys.argv[1]
+    goal_city = sys.argv[2]
+
+    main(start_city, goal_city)
--- a_star_algo.py
+++ a_star_algo.py
@@ -1,3 +1,4 @@
+import heapq
 from city_graph import CityGraph
 
 class AStarAlgo(CityGraph):
@@ -6,5 +7,36 @@ class AStarAlgo(CityGraph):
         super().__init__(coords_file, map_file)
 
     
-    def search():
-        pass
+    def search(self, start, goal):
+        # Prio queue that stores (score, city, path)
+        queue = []
+        heapq.heappush(queue, (0 + self.haversine_distance(start, goal), 0, start, [start]))
+
+        # How much does it cost to go from city to next city
+        score = {city: float('inf') for city in self.graph}
+        score[start] = 0
+        visited = set()
+
+        while queue:
+            _, current_score, current_city, path = heapq.heappop(queue)
+
+            # Goal reached
+            if current_city == goal:
+                return path, current_score
+
+            visited.add(current_city)
+
+            for neighbor, dist in self.graph[current_city].items():
+                if neighbor in visited:
+                    continue
+
+                temp_score = current_score + dist
+                if temp_score < score[neighbor]:
+                    score[neighbor] = temp_score
+                    f_score = temp_score + self.haversine_distance(neighbor, goal)
+                    heapq.heappush(queue, (f_score, temp_score, neighbor, path + [neighbor]))
+
+        return None, float('inf')  # If no path foun
+
+
+
